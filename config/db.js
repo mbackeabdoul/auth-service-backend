fe2a5a4fa31737c0974db45b6a349cebd4a3c0ec
Abdou Khoudoss <khoudossmbacke18@gmail.com>
2025-01-21 17:44:38 +0000

    first commit

---
+++ config/db.js
@@ -0,0 +1,19 @@
+// const mongoose = require('mongoose');
+// require('dotenv').config();
+
+// const connectDB = async () => {
+//   try {
+//     mongoose.connect(process.env.MONGODB_URI)
+//     .then(() => {
+//       console.log('Connexion à MongoDB réussie');
+//     })
+//     .catch((error) => {
+//       console.error('Erreur de connexion à MongoDB:', error);
+//     });
+//     console.error(`Erreur: ${error.message}`);
+//     process.exit(1);
+//   }
+// };
+
+
+// module.exports = connectDB;
